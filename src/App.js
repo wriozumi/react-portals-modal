27770864d3e7c5177c40ec6131660ff58c25e92c
wriozumi <tshatmanaliev@gmail.com>
2020-09-08 09:22:08 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import UIModal from "./Modal.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  toggleModal = () => {
+    this.setState((prevState) => ({ open: !prevState.open }));
+  };
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h2>React Portal Example</h2>
+        <button id="open-button" onClick={this.toggleModal}>
+          Toggle Modal
+        </button>
+        <UIModal show={open} onHide={this.toggleModal}>
+          A portal example
+        </UIModal>
+      </div>
+    );
+  }
 }
 
 export default App;
